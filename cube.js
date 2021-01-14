18478630a1450e4acab093f2105efa2679d35056
Gabriela Correa <gcc73@cornell.edu>
2021-01-14 16:00:43 -0500

    title updated

--- cube.js
+++ cube.js
@@ -14,29 +14,9 @@ camera.position.z = 5;
 
 const animate = function () {
     requestAnimationFrame( animate );
-
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
     renderer.render( scene, camera );
 };
-
-
-const loader = new THREE.FontLoader();
-
-loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-
-	const geometry = new THREE.TextGeometry( 'Hello three.js!', {
-		font: font,
-		size: 80,
-		height: 5,
-		curveSegments: 12,
-		bevelEnabled: true,
-		bevelThickness: 10,
-		bevelSize: 8,
-		bevelOffset: 0,
-		bevelSegments: 5
-	} );
-} );
 
 animate();
